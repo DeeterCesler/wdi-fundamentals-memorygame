16188441556cc21124b01cbfa030a43f44977f28
Deeter <deeter.cesler@gmail.com>
2018-09-03 14:45:39 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+
+var myArray = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = myArray[0];
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = myArray[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else alert("Sorry, try again.");
+}

506181f625b4e9f90fd640bac253076f41b5d0e7
Deeter <deeter.cesler@gmail.com>
2018-09-03 15:13:04 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,27 @@
 console.log("Up and running!");
 
 
-var myArray = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = myArray[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = myArray[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+function checkForMatch() {
+	/*I decided to use my own code here since I had already written 
+	a check for match function within flipCard. So I just moved it here*/
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		}
+		else alert("Sorry, try again.");
 	}
-	else alert("Sorry, try again.");
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+//
